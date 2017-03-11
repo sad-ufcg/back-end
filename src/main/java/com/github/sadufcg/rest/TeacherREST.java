ce6e751df0cd8622b3a7cd8d43eb5bd1a500b63c
Dev <dev@Antuness-MacBook-Air.local>
2017-03-11 12:12:05 -0300

    Teacher's REST finished

--- src/main/java/com/github/sadufcg/rest/TeacherREST.java
+++ src/main/java/com/github/sadufcg/rest/TeacherREST.java
@@ -1,7 +1,11 @@
 package com.github.sadufcg.rest;
 
 import com.github.sadufcg.pojo.Teacher;
-import org.springframework.web.bind.annotation.RestController;
+import com.github.sadufcg.services.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 /**
  * This controller provides the public API that is used to perform operations
@@ -11,5 +15,52 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@RequestMapping("/teacher")
 public class TeacherREST {
+
+    private final TeacherService service;
+
+    @Autowired
+    TeacherREST(TeacherService service) {
+        this.service = service;
+    }
+
+    @CrossOrigin
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
+    Teacher create(@RequestBody Teacher newTeacher) {
+        return service.create(newTeacher);
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    Teacher findOne(@PathVariable("id") String id) {
+        return service.findById(id);
+    }
+
+    @CrossOrigin
+    @RequestMapping(method = RequestMethod.GET)
+    List<Teacher> findAll() {
+        return service.findAll();
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    Teacher update(@PathVariable("id") String id, @RequestBody Teacher updatedTeacher) {
+        if (!id.equals(updatedTeacher.getSiape())) {
+            return null;
+        } else {
+            return service.update(updatedTeacher);
+        }
+    }
+
+    @CrossOrigin
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    void delete(@PathVariable("id") String id) {
+            Teacher teacher = service.findById(id);
+            if (teacher != null) {
+                service.delete(teacher);
+        }
+    }
+
 }
