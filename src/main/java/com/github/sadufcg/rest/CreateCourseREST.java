7b0ca59bd43ce8607e3bfaa099e98dcbd3ba6bdb
Antunes Dantas <antunes.silva@ccc.ufcg.edu.br>
2017-03-30 21:40:03 -0300

    atualizando repositorio local

---
+++ src/main/java/com/github/sadufcg/rest/CreateCourseREST.java
@@ -0,0 +1,31 @@
+package com.github.sadufcg.rest;
+
+import com.github.sadufcg.services.CsvClassCreator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by Antunes Dantas on 17/03/17.
+ */
+
+@RestController
+@RequestMapping("/create/upload")
+public class CreateCourseREST {
+
+    private final CsvClassCreator service;
+
+    @Autowired
+    public CreateCourseREST(CsvClassCreator service) {
+        this.service = service;
+    }
+
+    RequestMapping(method = RequestMethod.POST)
+    void createCourse(@RequestParam("file") MultipartFile file) {
+
+    }
+
+}
