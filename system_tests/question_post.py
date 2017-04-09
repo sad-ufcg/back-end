f4b4b08b1ba7545e470f1377e717695590ff290f
Matheus Gaudencio do Rêgo <matheusgr@gmail.com>
2017-04-09 11:02:35 -0300

    Unificando arquivo de teste.

--- system_tests/question_post.py
+++ system_tests/question_post.py
@@ -8,24 +8,47 @@ def _debug(r):
         print(r.reason)
         print(r.raw)
 
-def send(resource, json):
-    r = requests.post('http://localhost:8080/' + resource, json=json, auth=HTTPBasicAuth('user', 'pass'))
+def send(resource, json, secure=True):
+    if secure:
+        r = requests.post('http://localhost:8080/' + resource, json=json, auth=HTTPBasicAuth('user', 'pass'))
+    else:
+        r = requests.post('http://localhost:8080/' + resource, json=json)
     _debug(r)
+    return r
+
+def insecure_upload():
+    with open('example.csv', 'rb') as f:
+        r = requests.post('http://localhost:8080/upload', files={'file': f})
+        assert r.status_code == 401
+
+
+def upload():
+    with open('example.csv', 'rb') as f:
+        r = requests.post('http://localhost:8080/upload', files={'file': f}, auth=HTTPBasicAuth('user', 'pass'))
+        assert r.status_code == 200
+
 
 def questionario():
+    assert send('questionnaire', {}, False).status_code == 401
     send('questionnaire', {})
+
 
 def questoes():
     #TEXTO, MULTIPLA_ESCOLHA, SELECAO
+    assert send('question', {'enunciado': "1", 'tipoResposta': 'TEXTO', 'comentario': "", "questionnaire": 1}, False).status_code == 401
     send('question', {'enunciado': "Banana frita é bom?", 'tipoResposta': 'TEXTO', 'comentario': "", "questionnaire": 1})
     send('question', {'enunciado': "Rabanada é bom?", 'tipoResposta': 'MULTIPLA_ESCOLHA', 'comentario': "-", "questionnaire": 1})
     send('question', {'enunciado': "Rabanada é doce?", 'tipoResposta': 'SELECAO', 'comentario': ":)", "questionnaire": 1})
 
+
 def mailman():
     #TEXTO, MULTIPLA_ESCOLHA, SELECAO
+    assert send('mailman', {"questionnaire": 1}, False).status_code == 401
     send('mailman', {"questionnaire": 1})
 
 
+insecure_upload()
+upload()
 questionario()
 questoes()
 mailman()
