ca770dc71215d2e7f2c27cfa295bd2aa5829f7db
Lucas Silva <lucashenrique125@gmail.com>
2017-11-02 18:17:03 -0300

    Criação da entidade Questão

---
+++ src/main/java/com/ufcg/sad/models/Questao.java
@@ -0,0 +1,111 @@
+package com.ufcg.sad.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Questao {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column
+	@NotNull
+	private String enunciado;
+	
+	@Column
+	@NotNull
+	@Enumerated(EnumType.STRING)
+	private TipoResposta tipoResposta;
+	
+	@Column
+	@NotNull
+	private String comentario;
+	
+	public Questao(Long id, String enunciado, TipoResposta tipoResposta, String comentario) {
+		this.id = id;
+		this.enunciado = enunciado;
+		this.tipoResposta = tipoResposta;
+		this.comentario = comentario;
+	}
+	
+	public Questao() { }
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getEnunciado() {
+		return enunciado;
+	}
+	
+	public void setEnunciado(String enunciado) {
+		this.enunciado = enunciado;
+	}
+	
+	public TipoResposta getTipoResposta() {
+		return tipoResposta;
+	}
+	
+	public void setTipoResposta(TipoResposta tipoResposta) {
+		this.tipoResposta = tipoResposta;
+	}
+	
+	public String getComentario() {
+		return comentario;
+	}
+	
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((comentario == null) ? 0 : comentario.hashCode());
+		result = prime * result + ((enunciado == null) ? 0 : enunciado.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((tipoResposta == null) ? 0 : tipoResposta.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Questao other = (Questao) obj;
+		if (comentario == null) {
+			if (other.comentario != null)
+				return false;
+		} else if (!comentario.equals(other.comentario))
+			return false;
+		if (enunciado == null) {
+			if (other.enunciado != null)
+				return false;
+		} else if (!enunciado.equals(other.enunciado))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (tipoResposta != other.tipoResposta)
+			return false;
+		return true;
+	}
+}
