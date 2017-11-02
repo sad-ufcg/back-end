c3f09fa38e381a4fa16ea0c4c5ca73051a27482a
Lucas Silva <lucashenrique125@gmail.com>
2017-11-02 20:07:41 -0300

    Criação da entidade Questionário e do relacionamento com Questões

--- src/main/java/com/ufcg/sad/models/Questao.java
+++ src/main/java/com/ufcg/sad/models/Questao.java
@@ -1,16 +1,21 @@
 package com.ufcg.sad.models;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
 @Entity
-public class Questao {
+public class Questao implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
