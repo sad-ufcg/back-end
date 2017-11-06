ec384e650a202171d0faec3e2106b2d10033d955
Marianne Linhares Monteiro <mariannelinharesm@gmail.com>
2017-11-06 20:39:26 -0300

    Adicionando path para futuros testes de services

---
+++ src/test/java/com/ufcg/sad/services/questao/QuestaoServiceTest.java
@@ -0,0 +1,21 @@
+package com.ufcg.sad.services.questao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Testes para o Serviço uma questão.
+ * 
+ * @author Marianne Linhares
+ */
+public class QuestaoServiceTest {
+
+	//TODO: @mari-linhares adicionar testes assim que mock estiver configurado.
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/com/ufcg/sad/services/questionario/QuestionarioServiceTest.java
@@ -0,0 +1,22 @@
+package com.ufcg.sad.services.questionario;
+
+import static org.junit.Assert.*;
+
+/**
+ * Testes para o serviço de um questionário.
+ * 
+ * @author Marianne Linhares
+ */
+import org.junit.Test;
+
+
+public class QuestionarioServiceTest {
+	
+	//TODO: @mari-linhares adicionar testes assim que mock estiver configurado.
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
+
