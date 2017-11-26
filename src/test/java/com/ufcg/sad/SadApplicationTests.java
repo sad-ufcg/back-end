43b4a63118de31cf486ca3d468a636aa5053e472
Antunes Dantas <antunesdantas@Antuness-MacBook-Air.local>
2017-11-26 11:27:49 -0300

    corrigindo testes

--- src/test/java/com/ufcg/sad/SadApplicationTests.java
+++ src/test/java/com/ufcg/sad/SadApplicationTests.java
@@ -2,15 +2,12 @@ package com.ufcg.sad;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.ContextHierarchy;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Template de testes para a aplicação
  */
 @RunWith(SpringRunner.class)
-@ContextHierarchy(@ContextConfiguration)
 public class SadApplicationTests {
 
 	@Test
--- src/test/java/com/ufcg/sad/controller/DisciplinaControllerTest.java
+++ src/test/java/com/ufcg/sad/controller/DisciplinaControllerTest.java
@@ -1,17 +1,26 @@
 package com.ufcg.sad.controller;
 
+import com.ufcg.sad.SadApplication;
 import com.ufcg.sad.SadApplicationTests;
-import com.ufcg.sad.controllers.DisciplinaController;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-@WebMvcTest(DisciplinaController.class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = SadApplication.class)
+@TestPropertySource(
+        locations = "classpath:application.properties")
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
 public class DisciplinaControllerTest extends SadApplicationTests {
 
     @Autowired
@@ -21,7 +30,7 @@ public class DisciplinaControllerTest extends SadApplicationTests {
     public void verificaGetAll() throws Exception {
         mockMvc.perform(get("/disciplinas")
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isFound());
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
     }
 
 }
