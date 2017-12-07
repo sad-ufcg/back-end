794edff9cf5c6bffc8861bcd33f935058f7ba983
Marianne Linhares Monteiro <mariannelinharesm@gmail.com>
2017-12-06 21:45:38 -0300

    Adicionando equals a Token

--- src/main/java/com/ufcg/sad/models/token/Token.java
+++ src/main/java/com/ufcg/sad/models/token/Token.java
@@ -53,4 +53,37 @@ public class Token {
         this.questionarioAplicado = questionarioAplicado;
     }
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((questionarioAplicado == null) ? 0 : questionarioAplicado.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Token other = (Token) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (questionarioAplicado == null) {
+			if (other.questionarioAplicado != null)
+				return false;
+		} else if (!questionarioAplicado.equals(other.questionarioAplicado))
+			return false;
+		return true;
+	}
+    
+    
+
 }
